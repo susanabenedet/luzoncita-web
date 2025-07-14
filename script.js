bfe7121381c746a396100951eaa696395bd5a1f0
susanabenedet <susanabenedet95@gmail.com>
2025-07-14 14:09:49 +0200

    Primer commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".mySwiper", {
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    speed: 800,
+    effect: "slide",
+  });
+  
